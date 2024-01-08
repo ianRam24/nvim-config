aeee5db9c05dc78f5ad174bdc486ca27d409ade3
Ian Ramirez <Ian.Espinosa@jala.university>
2024-01-07 19:30:42 -0600

    Adding support for tailwind

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,21 @@
+return {
+    'goolord/alpha-nvim',
+    config = function ()
+       local alpha = require('alpha')
+       local dashboard = require('alpha.themes.dashboard')
+       dashboard.section.header.val = {
+      [[       _,    _   _    ,]],
+[[  .o888P     Y8o8Y     Y888o]],
+[[ d88888      88888      88888b ]],
+[[d888888b_  _d88888b_  _d888888b ]],
+[[8888888888888888888888888888888 ]],
+[[8888888888888888888888888888888 ]],
+[[YJGS8P"Y888P"Y888P"Y888P"Y8888P ]],
+ [[Y888   '8'   Y8P   '8'   888Y ]],
+  [['8o          V          o8' ]],
+   [[ `                     ` ]]
+
+    }
+      alpha.setup(dashboard.opts)
+    end
+};
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,14 @@
+return
+ {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "muniftanjim/nui.nvim",
+      "3rd/image.nvim",
+    },
+    config = function()
+      vim.keymap.set('n', '<C-b>', ':Neotree toggle <CR>')
+             end
+}
